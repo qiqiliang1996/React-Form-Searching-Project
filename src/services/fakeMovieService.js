f94ad195712654f1b495cc95cb81e4e4e928a4cd
qiqi liang <qlian100@syr.edu>
2021-12-12 18:11:32 -0500

    Dec12-3

--- src/services/fakeMovieService.js
+++ src/services/fakeMovieService.js
@@ -41,101 +41,101 @@ const movies = [
   {
     _id: "5b21ca3eeb7f6fbccd47181b",
     title: "6 My Chubby is so Cute",
-    genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller"},
     numberInStock: 6,
     dailyRentalRate: 3.5
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181e",
     title: "7 Holly Chubby Qiqi",
-    genre: {  _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller"},
     numberInStock: 7,
     dailyRentalRate: 4.5
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd47181f",
-    title: "8 Hell Yeah Chubby is One",
-    genre: {  _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
-    numberInStock: 8,
-    dailyRentalRate: 3.5
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd471821",
-    title: "9 The kiki",
-    genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy"},
-    numberInStock: 9,
-    dailyRentalRate: 3.5
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd47182111",
-    title: "10 The qiqi",
-    genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
-    numberInStock: 10,
-    dailyRentalRate: 3.5
-  },
-    {
-    _id: "5b21ca3eeb7f6fbccd471822222",
-    title: "11 The Love",
-    genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy"},
-    numberInStock: 11,
-    dailyRentalRate: 3.5
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd471823333",
-    title: "12 The Bae Chubby",
-    genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy"},
-    numberInStock: 12,
-    dailyRentalRate: 3.5
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd47182111114",
-    title: "13 The Sad Story",
-    genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy"},
-    numberInStock: 13,
-    dailyRentalRate: 3.5
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd47182111115",
-    title: "14 The Story of Chubby",
-    genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
-    numberInStock: 14,
-    dailyRentalRate: 3.5
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd47182111116",
-    title: "15 This Website is for Chubby",
-    genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy"},
-    numberInStock: 15,
-    dailyRentalRate: 3.5
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd47182111117",
-    title: "16 The Beautiful Cat",
-    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller"},
-    numberInStock: 16,
-    dailyRentalRate: 3.5
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd47182111117",
-    title: "17 The Wonderful Day",
-    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller"},
-    numberInStock: 17,
-    dailyRentalRate: 3.5
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd47182111117",
-    title: "18 The Start",
-    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller"},
-    numberInStock: 18,
-    dailyRentalRate: 3.5
-  },
-  {
-    _id: "5b21ca3eeb7f6fbccd47182111117",
-    title: "19 Changing is Difficult",
-    genre: {_id: "5b21ca3eeb7f6fbccd471820", name: "Thriller"},
-    numberInStock: 19,
-    dailyRentalRate: 3.5
-  }
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd47181f",
+  //   title: "8 Hell Yeah Chubby is One",
+  //   genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy"},
+  //   numberInStock: 8,
+  //   dailyRentalRate: 3.5
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd471821",
+  //   title: "9 The kiki",
+  //   genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy"},
+  //   numberInStock: 9,
+  //   dailyRentalRate: 3.5
+  // }
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd47182111",
+  //   title: "10 The qiqi",
+  //   genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+  //   numberInStock: 10,
+  //   dailyRentalRate: 3.5
+  // },
+  //   {
+  //   _id: "5b21ca3eeb7f6fbccd471822222",
+  //   title: "11 The Love",
+  //   genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy"},
+  //   numberInStock: 11,
+  //   dailyRentalRate: 3.5
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd471823333",
+  //   title: "12 The Bae Chubby",
+  //   genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy"},
+  //   numberInStock: 12,
+  //   dailyRentalRate: 3.5
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd47182111114",
+  //   title: "13 The Sad Story",
+  //   genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy"},
+  //   numberInStock: 13,
+  //   dailyRentalRate: 3.5
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd47182111115",
+  //   title: "14 The Story of Chubby",
+  //   genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
+  //   numberInStock: 14,
+  //   dailyRentalRate: 3.5
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd47182111116",
+  //   title: "15 This Website is for Chubby",
+  //   genre: {  _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy"},
+  //   numberInStock: 15,
+  //   dailyRentalRate: 3.5
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd47182111117",
+  //   title: "16 The Beautiful Cat",
+  //   genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller"},
+  //   numberInStock: 16,
+  //   dailyRentalRate: 3.5
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd47182111117",
+  //   title: "17 The Wonderful Day",
+  //   genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller"},
+  //   numberInStock: 17,
+  //   dailyRentalRate: 3.5
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd47182111117",
+  //   title: "18 The Start",
+  //   genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller"},
+  //   numberInStock: 18,
+  //   dailyRentalRate: 3.5
+  // },
+  // {
+  //   _id: "5b21ca3eeb7f6fbccd47182111117",
+  //   title: "19 Changing is Difficult",
+  //   genre: {_id: "5b21ca3eeb7f6fbccd471820", name: "Thriller"},
+  //   numberInStock: 19,
+  //   dailyRentalRate: 3.5
+  // }
   
 ];
 
